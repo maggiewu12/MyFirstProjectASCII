9ef2bd3724cd1a19d58965e8b26610f61a1ba0f4
Maggie Wu <100169470@lubbockisd.net>
2022-09-06 08:35:12 -0500

    Initial commit

---
+++ src/MyFirstClass.java
@@ -0,0 +1,18 @@
+public class MyFirstClass {
+    public static void main(String[] args) {
+        System.out.println("___________________________");
+        System.out.println("|*************************|");
+        System.out.println("|*************************|");
+        System.out.println("|************____*********|");
+        System.out.println("|*********/.......\\*******|");
+        System.out.println("|*********|0.mmm.0|*******|");
+        System.out.println("|**********\\\\(----)*******|");
+        System.out.println("|*******/..vvvvvvv.*******|");
+        System.out.println("|****//=...........=\\\\****|");
+        System.out.println("|****/=.............=\\****|");
+        System.out.println("|******==.........==******|");
+        System.out.println("|********0--===--0********|");
+        System.out.println("|*******mMm*****mMm*******|");
+        System.out.println("---------------------------");
+    }
+}
